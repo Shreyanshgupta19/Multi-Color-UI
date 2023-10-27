b69a987db15c635ed0a0cecab0639e1dd2f85556
Shreyansh Gupta <gshreyansh62@gmail.com>
2023-10-27 19:41:19 +0530

    27.10.2023

---
+++ app/src/main/java/com/example/multicolorui/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.multicolorui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multicolorui.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
